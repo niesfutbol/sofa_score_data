93cf71e06a673d40dd69b73ba6f7b8c67fac390e
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-04-15 20:49:33 -0700

    🚧 Write output files

--- src/first_example.py
+++ src/first_example.py
@@ -2,7 +2,7 @@ import requests
 import sofa_score_data as ssd
 
 
-match_url = "https://www.sofascore.com/fc-juarez-club-puebla/QNszHFb#id:12154131"
+match_url = "https://www.sofascore.com/club-america-deportivo-toluca-fc/GNsON#id:11911066"
 
 sofa = ssd.Sofascore()
 
@@ -10,3 +10,17 @@ players_and_id = sofa.get_player_ids(match_url)
 id_playes = list(players_and_id.values())
 salidita = sofa.get_match_shotmap(match_url)
 shotmap = salidita.json()["shotmap"]
+momento = sofa.match_momentum(match_url)
+team_names = sofa.get_team_names(match_url)
+
+
+def write_team_name(value):
+    if value > 0:
+        return team_names[0]
+    return team_names[1]
+
+
+team_name = [write_team_name(value) for value in list(momento.value)]
+momento["team_name"] = team_name
+output_name = f"momentum_{team_names[0].lower().replace(' ','-')}_vs_{team_names[1].lower().replace(' ','-')}.csv"
+momento.to_csv(output_name, index=False)
--- src/fotmob.py
+++ src/fotmob.py
@@ -2,13 +2,32 @@ import requests
 import pandas as pd
 import json
 from bs4 import BeautifulSoup
-match_url = "https://www.fotmob.com/matches/puebla-vs-fc-juarez/2r8ak7wr#4384438"
+
+match_url = "https://www.fotmob.com/matches/liverpool-vs-crystal-palace/2tmp8g#4193856"
 r = requests.get(match_url)
 html = r.text
 soup = BeautifulSoup(html, "html.parser")
-json_fotmob = json.loads(soup.find("script", attrs={"id":"__NEXT_DATA__"}).contents[0])
+json_fotmob = json.loads(soup.find("script", attrs={"id": "__NEXT_DATA__"}).contents[0])
 shotmap = json_fotmob["props"]["pageProps"]["content"]["shotmap"]
 xG = pd.DataFrame(shotmap["shots"])
-xG.to_csv("xg_puebla_juarez.csv", index = False)
+home_team = json_fotmob["props"]["pageProps"]["general"]["homeTeam"]
+away_team = json_fotmob["props"]["pageProps"]["general"]["awayTeam"]
+home_team_name = home_team["name"].lower().replace(" ", "-")
+away_team_name = away_team["name"].lower().replace(" ", "-")
+output_path = f"xg_{home_team_name}_vs_{away_team_name}.csv"
 
-statistics = json_fotmob["props"]["pageProps"]["content"]["stats"]["Periods"]["All"]["stats"][0]["stats"]
+
+def write_team_name(teamId):
+    if teamId == home_team["id"]:
+        return home_team["name"]
+    return away_team["name"]
+
+
+team_name = [write_team_name(value) for value in list(xG.teamId)]
+xG["team_name"] = team_name
+xG.to_csv(output_path, index=False)
+
+statistics = json_fotmob["props"]["pageProps"]["content"]["stats"]["Periods"]["All"]["stats"][0][
+    "stats"
+]
+pd.DataFrame(statistics).to_csv("statistics.csv", index=False)
