eff2612c196be1f6708c2105b9b121651ee991e8
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-10 16:19:20 -0700

    ✅ Add other example

--- tests/test_fotbom.py
+++ tests/test_fotbom.py
@@ -13,10 +13,13 @@ def test_get_matches_url() -> None:
     match_urls: list = ssd.get_matches_url(league_info)
     assert len(match_urls) == 164, "List len is right"
     assert match_urls[0] == "/matches/queretaro-fc-vs-toluca/lto5f#4384303", "First url is right"
-    assert match_urls[1] == "/matches/atletico-de-san-luis-vs-mazatlan-fc/8tzh1yse#4384305", "Second url is right"
+    assert (
+        match_urls[1] == "/matches/atletico-de-san-luis-vs-mazatlan-fc/8tzh1yse#4384305"
+    ), "Second url is right"
 
 
 def test_get_matches_id() -> None:
     league_info: dict = load_data_from_leagues()
     match_ids: list = ssd.get_matches_id(league_info)
     assert match_ids[0] == "4384303", "First id is right"
+    assert match_ids[5] == "4384301", "Sixth id is right"
