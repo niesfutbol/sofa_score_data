4bae2e010b8c8ecf84030688204d4e7e13fb4e29
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-10 15:19:19 -0700

    🛑🧪 Test `get_matches_url()`: takes positional argument

--- tests/test_fotbom.py
+++ tests/test_fotbom.py
@@ -1,11 +1,14 @@
 import json
 import sofa_score_data as ssd
 
+
 # Opening JSON fil
 def load_data_from_leagues() -> dict:
     f = open("/workdir/tests/data/leagues_fotmob.json")
     return json.load(f)
 
+
 def test_get_matches_url() -> None:
     league_info: dict = load_data_from_leagues()
-    match_urls: list = ssd.get_matches_url()
+    match_urls: list = ssd.get_matches_url(league_info)
+    assert len(match_urls) == 164, "List len is right"
