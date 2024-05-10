3f4c600d786f58e538bef4a02e0ea18ebc16de66
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-10 16:08:10 -0700

    ♻️ Extract function `_get_all_matches_info()`

--- sofa_score_data/fotmob.py
+++ sofa_score_data/fotmob.py
@@ -1,6 +1,12 @@
 def get_matches_url(league_info: dict) -> list:
-    return [match["pageUrl"] for match in league_info["matches"]["allMatches"]]
+    all_matches = _get_all_matches_info(league_info)
+    return [match["pageUrl"] for match in all_matches]
 
 
 def get_matches_id(league_info: dict) -> list:
-    return [match["id"] for match in league_info["matches"]["allMatches"]]
+    all_matches = _get_all_matches_info(league_info)
+    return [match["id"] for match in all_matches]
+
+
+def _get_all_matches_info(league_info) -> list:
+    return [match for match in league_info["matches"]["allMatches"]]
