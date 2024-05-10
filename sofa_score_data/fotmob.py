58d4cb90db1016c6f4d465cc5784034b874ffeb7
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-10 16:03:23 -0700

    ✅ Pass test

--- sofa_score_data/fotmob.py
+++ sofa_score_data/fotmob.py
@@ -1,2 +1,6 @@
 def get_matches_url(league_info: dict) -> list:
     return [match["pageUrl"] for match in league_info["matches"]["allMatches"]]
+
+
+def get_matches_id(league_info: dict) -> list:
+    return [match["id"] for match in league_info["matches"]["allMatches"]]
