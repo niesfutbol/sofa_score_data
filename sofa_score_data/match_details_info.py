f7e9c8ed8b1f733e14008f0417658a0cc39ef10f
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-10 17:09:35 -0700

    ✅😇 Pass test

--- sofa_score_data/match_details_info.py
+++ sofa_score_data/match_details_info.py
@@ -2,5 +2,5 @@ def get_attendance(match_details) -> int:
     return match_details["content"]["matchFacts"]["infoBox"]["Attendance"]
 
 
-def get_top_players() -> None:
+def get_top_players(match_details) -> None:
     pass
