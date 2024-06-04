ce39945f01d7f92a6b9d7be804867ef58885ea1e
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-03 20:50:55 -0700

    ✅ Pass `obtain_not_downloaded_files()`

--- sofa_score_data/aux.py
+++ sofa_score_data/aux.py
@@ -8,3 +8,7 @@ def obtain_downloaded_files(path) -> list:
     fixture = [int(f.split(".")[0].split("_")[-1]) for f in onlyfiles if f.split(".")[1] == "json"]
     fixture = list(set(fixture))
     return fixture
+
+
+def obtain_not_downloaded_files(all_files, all_downloaded_files) -> list:
+    return [int(match) for match in all_files if int(match) not in all_downloaded_files]
