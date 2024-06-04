98df071e9674499d3fba2ea2b5051047c37edd24
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-03 20:46:31 -0700

    🗃️✅ Add data test files

--- tests/test_aux.py
+++ tests/test_aux.py
@@ -5,3 +5,11 @@ def test_obtain_downloaded_files() -> None:
     expected_id: list = [4230530, 4230531, 4230532]
     path: str = "/workdir/tests/data/downloaded_examples/"
     obtained: list = ssd.obtain_downloaded_files(path)
+    assert obtained == expected_id
+
+def test_obtain_not_downloaded_files() -> None:
+    all_downloaded_files: list = [4230530, 4230531, 4230532]
+    all_files: list = ["4230530", 4230531, "4230532", "4230533", 4230534]
+    expected: list = [4230533, 4230534]
+    obtained: list = ssd.obtain_not_downloaded_files(all_files, all_downloaded_files)
+    assert obtained == expected
