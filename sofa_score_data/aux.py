71502d59ff78d2dd18aac4bbe18c891cafd8d245
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-03 20:11:51 -0700

    ✅ Pass test

--- sofa_score_data/aux.py
+++ sofa_score_data/aux.py
@@ -1,3 +1,10 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
 
 def obtain_downloaded_files(path) -> list:
-    ...
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    fixture = [int(f.split(".")[0].split("_")[-1]) for f in onlyfiles if f.split(".")[1] == "json"]
+    fixture = list(set(fixture))
+    return fixture
