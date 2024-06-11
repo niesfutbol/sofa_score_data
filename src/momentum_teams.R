4fd0a36c3ae05693e9868f24042a6816ce9ba097
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-10 22:12:55 -0700

    🚧 Fix just local

--- src/momentum_teams.R
+++ src/momentum_teams.R
@@ -5,6 +5,6 @@ momentum_path <- "/workdir/results/serie_a/momentum_home_in_match.csv"
 momentum <- read_csv(momentum_path, show_col_types = FALSE)
 
 momentum_team <- momentum |>
-  select(c(matchId, home_momentum = momentum_porc)) |>
-  mutate(away_momentum = 100 - home_momentum) |>
-  pivot_longer(!matchId, names_to = "local", values_to = "momentum")
+  select(c(matchId, momentum_home = momentum_porc)) |>
+  mutate(momentum_away = 100 - momentum_home) |>
+  pivot_longer(!matchId, names_to = "local", values_to = "momentum", names_prefix = "momentum_")
--- src/split_matches_table.R
+++ src/split_matches_table.R
@@ -5,13 +5,13 @@ fixtures_path <- "results/serie_a/general_league_match.csv"
 matches <- read_csv(fixtures_path, show_col_types = FALSE)
 
 matches_id <- matches |>
-  select(c(matchId, home_id, away_id)) |>
-  pivot_longer(!matchId, names_to = "local", values_to = "team_id") |>
+  select(c(matchId, id_home = home_id, id_away = away_id)) |>
+  pivot_longer(!matchId, names_to = "local", values_to = "team_id", names_prefix = "id_") |>
   write_csv("results/serie_a/match_teams_id.csv")
 
 matches_score <- matches |>
-  select(c(matchId, home_score, away_score)) |>
-  pivot_longer(!matchId, names_to = "local", values_to = "team_score") |>
+  select(c(matchId, score_home = home_score, score_away = away_score)) |>
+  pivot_longer(!matchId, names_to = "local", values_to = "team_score", names_prefix = "score_") |>
   write_csv("results/serie_a/match_score.csv")
 
 id_teams <- matches |>
