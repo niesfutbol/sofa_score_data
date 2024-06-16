19a338d9a08c4d105b4117ba3627f216ece61a91
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-16 01:39:11 -0700

    🚧 Calculate the momentum distribution

--- src/momentum_teams.R
+++ src/momentum_teams.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(comprehenr)
 
 
 momentum_path <- "/workdir/results/serie_a/momentum_home_in_match.csv"
@@ -14,3 +15,24 @@ matches_id <- read_csv("results/serie_a/match_teams_id.csv", show_col_types = FA
 full_momentum <- matches_id |>
   left_join(momentum_team, by = c("matchId", "local")) |>
   left_join(id_teams, by = c("team_id"="id"))
+
+calculate_mean_momentum <- function(momentum, team) {
+  momentum |>
+    filter(team_id == team) %>%
+    .$momentum |>
+    sample(replace = TRUE) |>
+    mean()
+}
+
+momentum_distribution <- tibble::tibble()
+just_id_teams <- id_teams$id |> unique()
+for (id_t in just_id_teams) {
+  boots_mean_momentum <- comprehenr::to_vec(for(i in 1:2000) calculate_mean_momentum(full_momentum, id_t))
+  momentum_distribution <- rbind(
+    momentum_distribution,
+    tibble::tibble("team_id"=id_t, "momentum" = boots_mean_momentum)
+    )
+  print(id_t)
+}
+momentum_distribution |>
+  write_csv("results/serie_a/momentum_distribution.csv")
