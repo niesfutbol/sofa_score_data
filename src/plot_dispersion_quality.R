e72b57abcfbe9db659d37fed8e60c0f28f2b81bd
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-24 21:33:36 -0700

    🚧📊 Plot scatter spread vs IQR

---
+++ src/plot_dispersion_quality.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(ggplot2)
+library(ggrepel)
+
+table <- read_csv("results/serie_a/league_table.csv", show_col_types = FALSE)
+id_teams <- read_csv("results/serie_a/id_teams.csv", show_col_types = FALSE) |>
+  distinct(id, .keep_all=TRUE)
+momentum_distribution <- read_csv("results/serie_a/momentum_distribution.csv", show_col_types = FALSE)
+spread_5 <- momentum_distribution |>
+  group_by(team_id) |>
+  summarize(iq=IQR(momentum), x = quantile(momentum, 0.05)) |>
+  left_join(id_teams, by=c("team_id"="id")) |>
+  left_join(table, by=c("team_id"="id"))
+spread_5 |> ggplot(aes(x = iq, y = x)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, col="#00AFBB") +
+  ylab("Cuantil del 5%") + xlab("rango intercuartil") +
+  theme_classic() +
+  geom_text_repel(aes(label = spread_5$name), size = 3.5)
+ggsave("spread_distribution.png")
