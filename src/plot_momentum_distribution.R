f154a47aed1d674cbc97f68485a6d9b247042a1b
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-24 21:32:55 -0700

    💄 Re order teams

--- src/plot_momentum_distribution.R
+++ src/plot_momentum_distribution.R
@@ -2,10 +2,15 @@ library(tidyverse)
 library(ggplot2)
 library(ggridges)
 
-id_teams <- read_csv("results/serie_a/id_teams.csv", show_col_types = FALSE)
+table <- read_csv("results/serie_a/league_table.csv", show_col_types = FALSE)
+id_teams <- read_csv("results/serie_a/id_teams.csv", show_col_types = FALSE) |>
+  distinct(id, .keep_all=TRUE)
 momentum_distribution <- read_csv("results/serie_a/momentum_distribution.csv", show_col_types = FALSE)
 momentum_distribution |>
   left_join(id_teams, by=c("team_id"="id")) |>
-  ggplot(aes(x = momentum, y = name, group = name)) +
-  geom_density_ridges(fill = "#00AFBB")
+  left_join(table, by=c("team_id"="id")) |>
+  ggplot(aes(x = momentum, y = reorder(name, pts), group = name)) +
+  geom_density_ridges(fill = "#00AFBB") +
+  theme_classic() +
+  xlab("Inclinación del momento") + ylab("")
 ggsave("momentum_distribution.png")
