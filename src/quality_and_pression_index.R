32c892f73f01b46ce1fae323975b6afeb6f4c011
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-07-01 21:47:25 -0700

    🚧 Add first version quality and pression index

---
+++ src/quality_and_pression_index.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+data <- read_csv("tests/data/quality_and_pression_index.csv", show_col_types = FALSE)
+
+full_model <- data |>
+  select(-team) |>
+  lm(formula = pts ~ .)
+forward_model <- step(full_model, direction = "forward", scope = formula(~ .))
+backward_model <- step(full_model, direction = "backward")
