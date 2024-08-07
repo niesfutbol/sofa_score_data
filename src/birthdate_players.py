633616876d9f0f4f4bf51c1077d903a600346ca7
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-06 17:19:04 -0700

    🚧 Obtain info of all players

--- src/birthdate_players.py
+++ src/birthdate_players.py
@@ -1,4 +1,5 @@
 import requests
+import sofa_score_data as ssd
 
 league_url: str = "https://www.fotmob.com/api/fixtures?id=44&season=2024"
 r = requests.get(league_url)
@@ -10,6 +11,10 @@ teams_url: str = [f"https://www.fotmob.com/api/teams?id={team_id}" for team_id i
 
 squats = [requests.get(team_url).json()["squad"] for team_url in teams_url]
 
-player_url = "https://www.fotmob.com/api/playerData?id=141762"
+team_players = {id: squat for (id, squat) in zip(teams_id, squats)}
 
-player_info = requests.get(player_url).json()
+team_players_id = {k:ssd.obtain_players_from_squat(v) for k, v in team_players.items()}
+
+url_players = [f"https://www.fotmob.com/api/playerData?id={player['id']}" for squat in team_players_id.values() for player in squat]
+
+players = [ssd.obtain_player_info(requests.get(url_player).json()) for url_player in url_players]
