fc6a1fffa967512f52556a50fe3fccde4a2d34af
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-07 21:00:27 -0700

    🚨 Pass linter

--- src/birthdate_players.py
+++ src/birthdate_players.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 import sofa_score_data as ssd
 
 league = {"copa_america": 44, "eurocopa": 50}
@@ -19,20 +20,24 @@ team_players = {id: squat for (id, squat) in zip(teams_id, squats)}
 
 for k, v in team_players.items():
     output_file: str = f"/workdir/results/{league_name}/team_{k}.json"
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         json.dump(v, f, ensure_ascii=False, indent=4)
 
-team_players_id = {k:ssd.obtain_players_from_squat(v["squad"]) for k, v in team_players.items()}
+team_players_id = {k: ssd.obtain_players_from_squat(v["squad"]) for k, v in team_players.items()}
 
 
-url_players = [f"https://www.fotmob.com/api/playerData?id={player['id']}" for squat in team_players_id.values() for player in squat]
+url_players = [
+    f"https://www.fotmob.com/api/playerData?id={player['id']}"
+    for squat in team_players_id.values()
+    for player in squat
+]
 
 players: list = []
 for url_player in url_players:
     player: dict = requests.get(url_player).json()
     players.append(ssd.obtain_player_info(player))
     output_file: str = f"/workdir/results/{league_name}/player_{player['id']}.json"
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         json.dump(player, f, ensure_ascii=False, indent=4)
 
 pd.DataFrame(players).to_csv(f"{league_name}_birthdate_players.csv")
