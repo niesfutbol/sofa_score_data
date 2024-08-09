f324d6ea9db6583f7ff560bfc2de373ae3f00b0f
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-08 21:22:14 -0700

    🚧 Add bundesliga id

--- src/example_fotmob.py
+++ src/example_fotmob.py
@@ -2,15 +2,16 @@ import json
 import requests
 import sofa_score_data as ssd
 
-
-league_id: int = 55
+league_name: str = "bundesliga"
+league: dict = {"copa_america": 44, "eurocopa": 50, "bundesliga": 54}
+league_id: int = league[league_name]
 league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&ccode3=MEX&season=2023%2F2024"
 r = requests.get(league_url)
 league_info = r.json()
 match_id: list = ssd.get_matches_id(league_info)
 last_five_matches = [
     requests.get(f"https://www.fotmob.com/api/matchDetails?matchId={id_m}").json()
-    for id_m in match_id[0:30]
+    for id_m in match_id
 ]
 xG = [shot["expectedGoals"] for shot in ssd.get_all_shots(last_five_matches[3])]
 result_path: str = ssd.RESULTS[league_id]
