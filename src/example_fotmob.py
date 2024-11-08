b6b74015915eb201f6f1b60176c6dd929726147c
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-11-07 17:55:40 -0800

    🚧👽 Add header to scrap api

--- src/example_fotmob.py
+++ src/example_fotmob.py
@@ -2,22 +2,24 @@ import json
 import requests
 import sofa_score_data as ssd
 
-league_name: str = "premierleague"
+league_name: str = "champions_league"
 league: dict = {
+    "champions_league": 42,
     "copa_america": 44,
     "eurocopa": 50,
     "bundesliga": 54,
     "laliga": 87,
     "premierleague": 47,
 }
+header: dict = requests.get("http://46.101.91.154:6006/").json()
 league_id: int = league[league_name]
-league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&ccode3=MEX&season=2023%2F2024"
-r = requests.get(league_url)
+league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&season=2024%2F2025"
+r = requests.get(league_url, headers=header)
 league_info = r.json()
 match_id: list = ssd.get_matches_id(league_info)
 last_five_matches = [
-    requests.get(f"https://www.fotmob.com/api/matchDetails?matchId={id_m}").json()
-    for id_m in match_id
+    requests.get(f"https://www.fotmob.com/api/matchDetails?matchId={id_m}", headers=header).json()
+    for id_m in match_id[:72]
 ]
 xG = [shot["expectedGoals"] for shot in ssd.get_all_shots(last_five_matches[3])]
 result_path: str = ssd.RESULTS[league_id]
