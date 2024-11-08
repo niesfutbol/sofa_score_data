26e06673c4826fdac1c44fad20e14522ddbde3d3
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-11-07 17:57:02 -0800

    🚧👽 Add header to scrap api

--- src/write_general_league_match_info.py
+++ src/write_general_league_match_info.py
@@ -4,9 +4,10 @@ import pandas as pd
 import sofa_score_data as ssd
 
 
-league_id: int = 55
-league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&ccode3=MEX&season=2023%2F2024"
-r = requests.get(league_url)
+header: dict = requests.get("http://46.101.91.154:6006/").json()
+league_id: int = 42
+league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&ccode3=MEX&season=2024%2F2025"
+r = requests.get(league_url, headers=header)
 league_info = r.json()
 id_matches: list = ssd.get_matches_id(league_info)
 
@@ -14,7 +15,7 @@ result_path: str = ssd.RESULTS[league_id]
 general_info: pd.DataFrame = pd.DataFrame()
 shots_players: pd.DataFrame = pd.DataFrame()
 momentum_matches: pd.DataFrame = pd.DataFrame()
-for id_match in id_matches:
+for id_match in id_matches[:72]:
     print(id_match)
     input_path = f"/workdir/{result_path}/match_details_data_{id_match}.json"
     with open(input_path) as f:
--- src/write_table_of_league.py
+++ src/write_table_of_league.py
@@ -3,9 +3,10 @@ import pandas as pd
 import sofa_score_data as ssd
 
 
-league_id: int = 55
-league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&ccode3=MEX&season=2023%2F2024"
-r = requests.get(league_url)
+league_id: int = 42
+header: dict = requests.get("http://46.101.91.154:6006/").json()
+league_url: str = f"https://www.fotmob.com/api/leagues?id={league_id}&season=2024%2F2025"
+r = requests.get(league_url, headers=header)
 league_info = r.json()
 
 league: pd.DataFrame = ssd.obtain_table_of_league(league_info)
